fbcfcca21721d99a4aad10fe98bbd155c0fb4316
Pedro <pedrohenriquecordeiro>
2020-06-29 15:51:01 -0300

    Adicionando o arquivo de funcoes auxiliares

--- code.py
+++ code.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
-TIMESTAMP = 10
 
 driver = webdriver.Chrome('chromedriver') 
 driver.get('https://br.investing.com/equities/')
