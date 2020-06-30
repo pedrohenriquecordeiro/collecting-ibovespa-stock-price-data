3fbc18fe08f429564fe3cd05c08be691158d12e9
Pedro <pedrohenriquecordeiro>
2020-06-30 16:21:56 -0300

    Fixing bug

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-MAXIMUM_TIME = 20 # 20 seconds
+MAXIMUM_TIME = 5 # 20 seconds
 
 driver = webdriver.Chrome('chromedriver') 
 driver.get('https://br.investing.com/equities/')
@@ -44,6 +44,17 @@ for tr in trs:
 with open('stocks_today.csv', 'a') as stock_file:
     stock_file.write(header[1:] + '\n')
 
+for tr in trs:
+    # anchor tag
+    a = tr.find_element_by_tag_name('a')
+    stock_name = a.get_attribute('textContent')
+
+    if 'ON' in stock_name:  # only stock ON
+        # td tag
+        td_last_price = tr.find_element_by_xpath('./td[3]')
+        last_price = td_last_price.get_attribute('textContent')
+        print(stock_name, last_price)
+
 # looping in rows of table
 while(True):
     # getting prices
@@ -54,17 +65,15 @@ while(True):
 
         if 'ON' in stock_name:  # only stock ON
             # td tag
-            td_last_price = tr.find_element_by_xpath('//td[3]')
+            td_last_price = tr.find_element_by_xpath('./td[3]')
             last_price = td_last_price.get_attribute('textContent')
             prices = str(prices) + ';' + str(last_price) 
 
     with open('stocks_today.csv', 'a') as stock_file: # saving in file csv
         stock_file.write(prices[1:] + '\n')
-    
-    print(prices[1:] + '\n')
-    prices = ''  # cleaning up
 
-    time.sleep(60 * 10) # 10 minutes
-    print('Waiting ...')
+    prices = ''  # cleaning up
+    time.sleep(60 * 1) # 1 minutes
+
 
 driver.quit()
