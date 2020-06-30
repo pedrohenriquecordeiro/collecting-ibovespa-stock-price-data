422e85162d7bcbe0eac5c4fec76651960b48d641
Pedro <pedrohenriquecordeiro>
2020-06-30 09:34:18 -0300

    Filtering ON stocks

--- code.py
+++ code.py
@@ -37,29 +37,34 @@ for tr in trs:
     # anchor tag
     a = tr.find_element_by_tag_name('a')
     stock_name = a.get_attribute('textContent')
-    header = str(header) + ';' + str(stock_name)
+    if 'ON' in stock_name: # only stock ON
+        header = str(header) + ';' + str(stock_name)
 
 # writting header of csv file
 with open('stocks_today.csv', 'a') as stock_file:
-    stock_file.write(header + '\n')
+    stock_file.write(header[1:] + '\n')
 
 # looping in rows of table
 while(True):
     # getting prices
     for tr in trs:
-        # td tag
-        td_last_price = tr.find_element_by_xpath('//td[3]')
-        last_price = td_last_price.get_attribute('textContent')
-        prices = str(prices) + ';' + str(last_price) 
+        # anchor tag
+        a = tr.find_element_by_tag_name('a')
+        stock_name = a.get_attribute('textContent')
 
-    with open('stocks_today.csv', 'a') as stock_file:
-        stock_file.write(prices + '\n')
+        if 'ON' in stock_name:  # only stock ON
+            # td tag
+            td_last_price = tr.find_element_by_xpath('//td[3]')
+            last_price = td_last_price.get_attribute('textContent')
+            prices = str(prices) + ';' + str(last_price) 
 
+    with open('stocks_today.csv', 'a') as stock_file: # saving in file csv
+        stock_file.write(prices[1:] + '\n')
+    
+    print(prices[1:] + '\n')
     prices = ''  # cleaning up
 
     time.sleep(60 * 10) # 10 minutes
-
-
-
+    print('Waiting ...')
 
 driver.quit()
